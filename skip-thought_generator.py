82da387e2ab324c6bde3e8529b60d1278f737df8
Mansi Joshi <44506333+mansijoshii@users.noreply.github.com>
2020-12-27 22:56:14 +0530

    Update skip-thought_generator.py

--- skip-thought_generator.py
+++ skip-thought_generator.py
@@ -2,27 +2,26 @@ from skipthoughts import skipthoughts
 import h5py
 
 def main():
-    model = skipthoughts.load_model()
-    encoder = skipthoughts.Encoder(model)
-
     caption_file = "captions.txt"
-    training_image_file = "training_images.txt"
+    training_image_file = "train_images4.txt"
 
     captions = []
     with open(caption_file) as f:
         line_list = f.read().split("\n")
+        line_list = line_list[7500:9000]
         f1 = open(training_image_file, "w")
-        for line in line_list:
-            img = line.split("\t")[0]
-            cap = line.split("\t")[1]
+        for i in range(len(line_list)):
+            img = line_list[i].split("\t")[0]
+            cap = line_list[i].split("\t")[1]
             if len(cap) > 0:
                 captions.append(cap)
                 f1.write(img + "\n")
         f1.close()
 
-    caption_vectors = encoder.encode(captions)
+    model = skipthoughts.load_model()
+    caption_vectors = skipthoughts.encode(model, captions)
 
-    h = h5py.File("caption_vectors.hdf5", "w")
+    h = h5py.File("/content/drive/MyDrive/train_caption_vectors4.hdf5", "w")
     h.create_dataset("vectors", data=caption_vectors)		
     h.close()
 
